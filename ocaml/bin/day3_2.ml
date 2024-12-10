f8df0a016842d7cd64981ceb15a795847010cd14
phillip <phillipswanepoel@gmail.com>
2024-12-10 23:12:22 +0200

    m

--- ocaml/bin/day3_2.ml
+++ ocaml/bin/day3_2.ml
@@ -13,8 +13,6 @@ let int3 =
 
 let mul =
   let k = sep_by1 (char ',') int3 in
-  (fun nums -> [List.map int_of_string nums |> List.fold_left ( * ) 1])
-  <$> 
   (string "mul(" *> k <* char ')')
 
 let _read_until p =
@@ -24,18 +22,3 @@ let drop_until p =
   fix (fun recurse ->
     p <|> any_char *> recurse
   )
-
-let parser = many (drop_until mul)
-
-let () =
-  let input = "mul(2,4)%&mul[3,7]!@^do_not_mul(5,5)en(mul(11,8)mul(8,5))" in
-  let result = parse_string ~consume:Prefix parser input in
-  match result with
-  (* | Ok res -> printf("%s") (res |> String.concat " ") *)
-  | Ok res -> (printf "%d ") (res |> List.flatten |> List.fold_left ( + ) 0 )
-  (* | Ok res -> (printf "%s") res *)
-  | Error err -> printf("ERROR: %s") err
-
-
-(* PART TWO *)
-(* -------------------------------------------------------------------------- *)
